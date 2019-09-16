b0a5bad81efe146ec1509ad72dd538d6a83183c4
lukesilvia <lukesilvia@gmail.com>
2019-09-16 16:13:05 +0900

    configure redis to connect from other hosts.

--- provisioning/redis.sh
+++ provisioning/redis.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+set -eu
+
 FLAG_FILE="/var/provisioned_redis"
+REDIS_CONF="/etc/redis/redis.conf"
 
 if [ -f $FLAG_FILE ]; then
   echo "redis is provisioned."
@@ -10,5 +13,12 @@ fi
 echo "Installing redis..."
 apt-get update > /dev/null 2>&1
 apt-get install -y redis > /dev/null 2>&1
+
+echo "Configuring enable accessing from other hosts..."
+sed -i -e 's/^\(bind 127.0.0.1 ::1\)/# \1/' $REDIS_CONF
+sed -i -e 's/^\(protected-mode\) yes/\1 no/' $REDIS_CONF
+
+echo "Restarting redis..."
+systemctl restart redis
 
 touch $FLAG_FILE
